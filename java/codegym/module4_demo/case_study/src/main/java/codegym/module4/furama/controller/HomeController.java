764fa907f744a69ec35128e4b1e950546f074475
Nguyen Ngoc Anh Quoc <quocnna@gmail.com>
2022-04-15 16:52:23 +0700

    no message

---
+++ java/codegym/module4_demo/case_study/src/main/java/codegym/module4/furama/controller/HomeController.java
@@ -0,0 +1,12 @@
+package codegym.module4.furama.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "user/index";
+    }
+}
