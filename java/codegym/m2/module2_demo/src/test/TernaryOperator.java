d3d2048e1a33b5b4c7bb7229420fe9da9488a4be
Nguyen Ngoc Anh Quoc <quocnna@gmail.com>
2022-05-13 11:46:28 +0700

    no message

---
+++ java/codegym/m2/module2_demo/src/test/TernaryOperator.java
@@ -0,0 +1,31 @@
+package test;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int diem = 12;
+
+        String kq = diem >= 8 ? "Giỏi"
+                : diem >= 6.5 ? "Khá"
+                : diem >= 5 ? "Trung bình"
+                : "Kém";
+
+//        String kq;
+//        if (diem >= 8)
+//            kq = "Giỏi";
+//        else if (diem >= 6.5)
+//            kq = "Khá";
+//        else if (diem >= 5)
+//            kq = "Trung bình";
+//        else
+//            kq = "Kém";
+
+        System.out.println(kq);
+
+        int year = 2022;
+        String ms = year % 4 ==0 ?
+                year % 100 == 0 ?
+                        year % 400 == 0 ? "nhuan" : "ko phai nhuan"
+                        : "nhuan"
+                : "ko phai nhuan";
+    }
+}
